f16bd0b06732c54638a4d31cf9b99013d609e6d0
r123198 <tibudan.rafael@gmail.com>
2026-06-30 23:55:43 +0800

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
-import nextPlugin from "@next/eslint-plugin-next";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-/** @type {import("eslint").Linter.Config[]} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
 const eslintConfig = [
-  {
-    ...nextPlugin.flatConfig.coreWebVitals,
-  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
